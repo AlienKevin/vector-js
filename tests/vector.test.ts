96b4fbcdb2e5b9dee0acaf58fe9609c07e55be8c
Kevin Li <kevinli020508@gmail.com>
2019-12-03 19:51:21 -0500

    Add 3 vector addition tests

--- tests/vector.test.ts
+++ tests/vector.test.ts
@@ -2,15 +2,26 @@ import { addVectors } from "../src/vector";
 
 const v1 = [1, 2];
 const v2 = [0, -3];
+const v3 = [-0.23, -10.05]
 
 describe("Test vector addition", () => {
-    test("vector addition return components", () =>
+    test("2 vector addition return components", () =>
         expect(addVectors([v1, v2], "com")).toEqual([1, -1])
     );
-    test("vector addition return angles", () =>
+    test("3 vector addition return components", () =>
+        expect(addVectors([v1, v2, v3], "com")).toEqual([0.77, -11.05])
+    );
+    test("2 vector addition return angles", () =>
         expect(addVectors([v1, v2], "ang")).toEqual({
             mag: Math.sqrt(2),
             dir: -45,
+            dim: 2
+        })
+    );
+    test("3 vector addition return angles", () =>
+        expect(addVectors([v1, v2, v3], "ang")).toEqual({
+            mag: Math.sqrt(0.77**2 + 11.05**2),
+            dir: -86.01388695010866,
             dim: 2
         })
     );
